7326579e8904a2eaea99a5f1eebd2824258d44c2
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-28 13:56:33 +0800

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,23 +1,34 @@
 public class Recursion{
    //part1: factorial non-recursion
    public static int factorial(int n){
-      for(int i=0; i<n;i++)
-         {
+      int m=1;
+      for(int i=1; i<n;i--)
+         {m*=i;}
+      return m;
 
    //part2: factorial recursion
-   public static void rec(int n) {
-		if(n==0)
-			return;
-		rec(n-1);
-	}
+   public static int factorial(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
    //part3: fibonacci sequence non-recursion
-   
-   
-   
+   public static int Fibonacci(int n){
+      if(n==1||n==2)
+	    return 1;
+      int one = 1;
+      int two = 1;
+      for(int i = 3; i <n; i ++){
+         if(i == n)
+            return one + two;
+         two+= one;
+         one = two;
+      }
+   }
    //part4: fibonacci sequence recursion
    public static int fibonacci(int n) {
-		if(n==1||n==2)
-			return 1;
-		return fibonacci(n-1)+fibonacci(n-2);
+         if(n==1||n==2)
+	    return 1;
+	return fibonacci(n-1)+fibonacci(n-2);
 	}
 }
